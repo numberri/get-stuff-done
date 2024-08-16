f464f53d4b4e08514932b0959a81e86155dc4c01
berri <cyberri@outlook.com>
2024-08-17 00:58:27 +1000

    Initial commit, adds command line parsing (even if it doesn't do anything now)

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(name = "get shit done")]
+#[command(version = "1.0")]
+#[command(about = "blocks websites and programs while you get work done", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// adds a website or program to the blocklisrt
+    Add {
+        /// specify a website to be added to the blacklist
+        #[arg(short, long)]
+        website: Option<String>,
+
+        /// specify a program ro be added to the blacklist
+        #[arg(short, long)]
+        program: Option<String>,
+    },
+
+    /// changes command to restart NetworkManager, networkctl, etc. this is to update /etc/hosts
+    Network { command: Option<PathBuf> },
+
+    /// starts a new session
+    Start {
+        #[arg(short, long)]
+        time: Option<String>,
+
+        #[arg(short, long, value_name = "FILE")]
+        file: Option<PathBuf>,
+    },
+
+    /// pauses current session for 5 minutes
+    Break,
+
+    /// determines if a session is running and checks conditions to unlock
+    Status,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+/*
+Using this as space to draft out what I want CLI arguments to look like
+
+Commands:
+    add <-w -p> <name>    adds a website or program to the blocklist
+    network-command <command>       changes command to restart network manager (updates /etc/hosts)
+
+    start <-t | -f | -h>            starts a new session
+    break                           pasues current session for 5 minutes - perhaps limit to 1-2 a session?
+    status                          determines if a session is running. if it is, check conditions
+                                    to unlock session
+
+    help                            displays a help message :)
+
+Options:
+    -t, --time, --timer <XHXM>      sets a timer to block websites. in the form e.g. 2h30m, 45m, 1h
+    -f --file <PATH> <NUM>          watches a file for difference in lines
+    -h, --help                      shows a help message :)
+
+no arguments will be equivlent to 'gsd start -t 1h'
+*/
